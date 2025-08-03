5b9e787c4a285f806bdc8f53ce5c72677b5b24fc
Lucky Singh <iqrapakistan838@gmail.com>
2025-08-03 15:11:38 +0000

    Difa add Link/https://i.ibb.co/67kgqFp4/IMG-20250803-WA0009-1.jpg

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -114,7 +114,7 @@ export default function Home() {
       title: 'DIFA',
       description: 'Diploma in Financial Accounting',
       icon: <Calculator className="h-8 w-8" />,
-      imageUrl: 'https://i.ibb.co/5hdDZj7D/IMG-20250803-WA0008.jpg',
+      imageUrl: 'https://i.ibb.co/67kgqFp4/IMG-20250803-WA0009-1.jpg',
       dataAiHint: 'financial accounting',
       color: 'from-green-500 to-green-700',
     },
