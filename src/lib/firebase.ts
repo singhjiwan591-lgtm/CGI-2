39fa0ae1ff43536d06d9cbc2d76a350ebf45d45d
Lucky Singh <iqrapakistan838@gmail.com>
2025-08-04 15:50:22 +0000

    FirebaseError: Firebase: Error (auth/invalid-api-key).     at createErro

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -6,13 +6,13 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
+  "projectId": "webfolio-unb29",
+  "appId": "1:120704760142:web:925456cffd2817de8a061f",
+  "storageBucket": "webfolio-unb29.firebasestorage.app",
+  "apiKey": "AIzaSyBxFNo_sOOM2uGHqzA1eqKFEbVR48wG1CM",
+  "authDomain": "webfolio-unb29.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "120704760142"
 };
 
 // Initialize Firebase
