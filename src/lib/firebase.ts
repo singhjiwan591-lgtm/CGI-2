064ede015a0a2296766151468fe08a62eb0bc251
Lucky Singh <iqrapakistan838@gmail.com>
2025-08-05 09:10:03 +0000

    // Import the functions you need from the SDKs you need import { initial

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,8 +1,6 @@
-
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -17,4 +15,8 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-export { app };
+// Initialize Firebase Authentication and get a reference to the service
+const auth = getAuth(app);
+
+
+export { app, auth };
