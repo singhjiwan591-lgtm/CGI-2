5dc1afa89346f5d4ae42675c4b3c54cea3cd5412
Lucky Singh <iqrapakistan838@gmail.com>
2025-08-06 08:19:17 +0000

    Our Identity/image link use/https://i.ibb.co/hJcs4VzM/Compress-JPEG-Onli

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -343,7 +343,7 @@ export default function Home() {
           </h2>
           <Card className="w-full max-w-sm p-8 bg-card transform transition-transform duration-500 hover:scale-110 hover:shadow-2xl hover:shadow-primary/30 flex justify-center items-center">
             <Image
-              src="https://i.ibb.co/dwFFgmGS/1000058336-removebg-preview.png"
+              src="https://i.ibb.co/hJcs4VzM/Compress-JPEG-Online-img.jpg"
               alt="Web an d App Logo"
               width={200}
               height={200}
