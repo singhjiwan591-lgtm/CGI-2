b265380c4f324980b58805f28c0dbdd8a7576d22
Lucky Singh <iqrapakistan838@gmail.com>
2025-08-12 08:00:33 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,5 +1,7 @@
 import { RegisterForm } from '@/components/register-form';
 
+export const dynamic = 'force-dynamic';
+
 export default function RegisterPage({ searchParams }: { searchParams?: { course?: string } }) {
   const selectedCourse = searchParams?.course;
   
