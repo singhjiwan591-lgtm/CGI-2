5c1f971ff92da9e87bcdf09f4af3c3a8edf85eb1
Lucky Singh <iqrapakistan838@gmail.com>
2025-08-12 16:01:55 +0000

    login failed

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -2,7 +2,7 @@
 'use client';
 
 // Import the functions you need from the SDKs you need
-import { initializeApp, getApp, getApps } from "firebase/app";
+import { initializeApp, getApp, FirebaseApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getMessaging, getToken } from "firebase/messaging";
@@ -20,8 +20,14 @@ const firebaseConfig = {
   measurementId: "G-NSDVPWMS6Q"
 };
 
-// Initialize Firebase for SSR, if not already initialized
-const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+// Initialize Firebase
+let app: FirebaseApp;
+try {
+  app = getApp();
+} catch (error) {
+  app = initializeApp(firebaseConfig);
+}
+
 
 const auth = getAuth(app);
 const db = getFirestore(app);
