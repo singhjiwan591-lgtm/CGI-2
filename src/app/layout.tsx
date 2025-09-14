7f8e8600529906cb6a8c7b3f264bf497f951491e
Lucky Singh <iqrapakistan838@gmail.com>
2025-09-14 17:22:07 +0000

    Ismein meta tag total dialo Google search engine wicked jaldi rank kare

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,9 +16,32 @@ export const metadata: Metadata = {
     template: '%s | Global Computer Institute',
   },
   description: 'Join Global Computer Institute in Jalalabad (West), Punjab for expert-led courses in Web Development, Data Science, Accounting, ADCA, CCA, and more. Start your tech career with us!',
-  keywords: ['Computer Institute Jalalabad', 'Global Computer Institute', 'Computer Courses', 'ADCA', 'DCA', 'Web Development Course', 'Learn HTML', 'Typing Course', 'Punjab Computer Education'],
+  keywords: [
+    'Computer Institute Jalalabad', 
+    'Global Computer Institute', 
+    'Computer Courses', 
+    'ADCA', 
+    'DCA', 
+    'Web Development Course', 
+    'Learn HTML', 
+    'Typing Course', 
+    'Punjab Computer Education',
+    'Best Computer Institute Punjab',
+    'Computer Centre Fazilka',
+    'Tally course',
+    'Graphic design course',
+    'Basic computer course',
+    'Computer coaching Jalalabad'
+  ],
   authors: [{ name: 'Global Computer Institute', url: 'https://i.ibb.co/5X00XdH9/0cbf6ee1-8add-4c4e-afdf-1d7eb2a4d1e7.png' }],
   creator: 'Global Computer Institute',
+  applicationName: 'Global Computer Institute',
+  referrer: 'origin-when-cross-origin',
+  formatDetection: {
+    email: false,
+    address: false,
+    telephone: false,
+  },
   icons: {
     icon: 'https://i.ibb.co/5X00XdH9/0cbf6ee1-8add-4c4e-afdf-1d7eb2a4d1e7.png',
     shortcut: 'https://i.ibb.co/5X00XdH9/0cbf6ee1-8add-4c4e-afdf-1d7eb2a4d1e7.png',
@@ -27,7 +50,8 @@ export const metadata: Metadata = {
   openGraph: {
     type: 'website',
     locale: 'en_IN',
-    url: 'https://i.ibb.co/5X00XdH9/0cbf6ee1-8add-4c4e-afdf-1d7eb2a4d1e7.png',
+    url: 'https://gci.edu.in', // Replace with your actual domain
+    siteName: 'Global Computer Institute',
     title: 'Global Computer Institute | Best Tech Education in Jalalabad',
     description: 'Empowering the next generation of tech leaders with practical skills and placement assistance.',
     images: [
@@ -43,11 +67,19 @@ export const metadata: Metadata = {
     card: 'summary_large_image',
     title: 'Global Computer Institute | Start Your Tech Career',
     description: 'Expert-led computer courses in Jalalabad, Punjab. Join us to build your future in technology.',
+    creator: '@yourtwitterhandle', // Replace with your Twitter handle
     images: ['https://i.ibb.co/6RdhCvSg/Compress-JPEG-Online-img-800x600.jpg'],
   },
   robots: {
     index: true,
     follow: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      'max-video-preview': -1,
+      'max-image-preview': 'large',
+      'max-snippet': -1,
+    },
   },
   assets: ['https://i.ibb.co/5X00XdH9/0cbf6ee1-8add-4c4e-afdf-1d7eb2a4d1e7.png/assets'],
   category: 'Education',
