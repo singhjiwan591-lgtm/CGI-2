6a9a8d7c385084efce5054ac80188bd7d1d76543
Lucky Singh <iqrapakistan838@gmail.com>
2025-09-28 15:22:29 +0000

    // Import the functions you need from the SDKs you need import { initial

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -6,12 +6,13 @@ import { getStorage } from "firebase/storage";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
+  apiKey: "AIzaSyChe50HhyCbWYgYIYsJWW_J3bN99TCtsOs",
+  authDomain: "storage-ced10.firebaseapp.com",
+  projectId: "storage-ced10",
+  storageBucket: "storage-ced10.appspot.com",
+  messagingSenderId: "428344436985",
+  appId: "1:428344436985:web:2602e4762c4be76c2fda28",
+  measurementId: "G-FVF1WN0D4F"
 };
 
 // Initialize Firebase
