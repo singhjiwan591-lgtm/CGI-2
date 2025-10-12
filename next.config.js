7761bb537c9712bd685942640cd257dc61419b54
Lucky Singh <iqrapakistan838@gmail.com>
2025-10-12 14:18:33 +0000

    speed chak  website

--- next.config.js
+++ next.config.js
@@ -32,8 +32,6 @@ const securityHeaders = [
 
 const withPWA = require('@ducanh2912/next-pwa').default({
   dest: 'public',
-  register: true,
-  skipWaiting: true,
   disable: process.env.NODE_ENV === 'development',
 });
 
